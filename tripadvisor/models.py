2277ac5d604940ea898bef02fc7edd3d8bc0333f
Maciej Borkowski <borkowski.mac@gmail.com>
2020-02-04 20:17:19 +0100

    Add Transportation model

--- tripadvisor/models.py
+++ tripadvisor/models.py
@@ -7,7 +7,6 @@ class Spot:
     """
     def __init__(self, address: str, identifier: str = "", baggage: str = ""):
         """
-
         :param address: address of place
         :param identifier: some specific identifier eg. gate 43 at airport, train platform 2
         :param baggage: place where baggage can be left
@@ -15,4 +14,22 @@ class Spot:
         self.address = address
         self.identifier = identifier
         self.baggage = baggage
+
+
+class Transportation:
+    """
+    Transportation to move from A to B.
+    """
+
+    categories = ['train', 'flight', 'bus']  # acceptable types of transportation
+
+    def __init__(self, category: categories, description: str = "", identifier: str = ""):
+        """
+        :param category: on of acceptable transportation type from Class variable 'categories'
+        :param description: additional information eg. city (for bus), charter (for plain)
+        :param identifier: to help find the transportation eg. bus plates, plain number
+        """
+        self.category = category
+        self.description = description
+        self.identifier = identifier
 
