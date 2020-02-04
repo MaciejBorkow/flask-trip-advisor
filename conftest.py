3b6d6d0bf81dc96093372b70c0838d135a325d55
Maciej Borkowski <borkowski.mac@gmail.com>
2020-02-04 20:35:32 +0100

    Add boardin card stack dict fixture for tests purpose

--- conftest.py
+++ conftest.py
@@ -13,3 +13,44 @@ def app():
 def client(app):
     return app.test_client()
 
+@pytest.fixture
+def valid_boarding_card_stack():
+
+    boarding_card_stack_dict = {
+        "stack": [{
+            "transportation": {
+                'category': 'train',
+                'description': 'airport',
+                'identifier': '123'
+            },
+            "departure": {
+                "address": "Dworzec Głowny, Poznan, Poland",
+                "identifier": "peron 7",
+                "baggage": "u kierowcy"
+            },
+            "destination": {
+                "address": "Dworzec Głowny, Warszawa, Poland",
+                "identifier": "peron 12",
+                "baggage": "u kierowcy"
+            }
+        },
+        {
+            "transportation": {
+                'category': 'train',
+                'description': 'airport',
+                'identifier': '123'
+            },
+            "departure": {
+                "address": "Dworzec Głowny, Poznan, Poland",
+                "identifier": "peron 7",
+                "baggage": "u kierowcy"
+            },
+            "destination": {
+                "address": "Dworzec Głowny, Warszawa, Poland",
+                "identifier": "peron 12",
+                "baggage": "u kierowcy"
+            }
+        }]
+    }
+
+    return copy.deepcopy(boarding_card_stack_dict)
